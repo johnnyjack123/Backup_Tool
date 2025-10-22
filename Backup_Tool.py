7f8008717596f61f476597b4d0ad84a9818d2e71
johnnyjack123 <jonathas.brandt@tutanota.de>
2025-10-22 12:54:19 +0200

    feat: Editable processes
    - Editable processes

--- Backup_Tool.py
+++ Backup_Tool.py
@@ -4,7 +4,7 @@ import shutil
 import time
 import json
 import logging
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import threading
 
 app = Flask(__name__)
@@ -40,71 +40,54 @@ def read():
 def ignore_backup(dir, contents):
     return ['backup'] if 'backup' in contents else []
 
-def backup_folders(folder_to_backup, folder_to_save_backup):
-    file = read()
-    backup_paths = file["backup_paths"]
-    for path in backup_paths:
-        folder_to_backup = path["folder_to_backup"]
-        base_backup_dir = path["folder_to_save_backup"]
-
+def backup_folders(folder_to_backup, base_backup_dir):
+    try:
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         folder_to_save_backup = base_backup_dir / f"backup_{timestamp}"
 
         shutil.copytree(src=str(folder_to_backup), dst=str(folder_to_save_backup), ignore=ignore_backup)
         print("Successfully copied.")
         logger.info("Successfully copied.")
-
-def backup():
-    try:
-        print("Backup Tool running.")
-        logger.info("Backup Tool running.")
-        while True:
-            file = read()
-            last_backup = file["last_backup"]
-            now = datetime.now()
-            if last_backup:
-                last_backup = datetime.fromisoformat(last_backup)
-                if datetime.now() - last_backup >= timedelta(days=1):
-                    backup_folders()
-                    file["last_backup"] = now.isoformat()
-                    save(file)
-                    print("Successfully did a Backup")
-                    logger.info("Successfully did a Backup")
-                time.sleep(86401.0)
-            else:
-                file["last_backup"] = now.isoformat()
-                save(file)
-                backup_folders()
-                print("Successfully did a Backup")
-                logger.info("Successfully did a Backup")
+        return True
     except Exception as e:
         logger.error(f"Error by backup: {e}", exc_info=True)
-
-if not data_file_path.exists():
-    default_content = {
-        "last_backup": ""
-    }
-    with open(data_file_path, 'w', encoding='utf-8') as f:
-        json.dump(default_content, f, indent=4)
+        return False
 
 def check_for_backup():
     while True:
         file = read()
         now = datetime.now()
         backup_paths = file["backup_paths"]
-        for entry, path in enumerate(backup_paths):
-            last_backup = path["last_backup"]
-            if last_backup:
-                if now - last_backup >= timedelta(days=1):
-
-            else:
-                file["backup_paths"][entry]["last_backup"] = now.isoformat()
-                save(file)
-        time.sleep(60)
+        if backup_paths:
+            for entry, path in enumerate(backup_paths):
+                last_backup = path["last_backup"]
+                if last_backup:
+                    backup_frequency = path["backup_frequency"]
+                    if now - datetime.fromisoformat(last_backup) >= timedelta(hours=backup_frequency):
+                        folder_to_backup = path["folder_to_backup"]
+                        folder_to_save_backup = path["folder_to_save_backup"]
+                        result = backup_folders(folder_to_backup, folder_to_save_backup)
+                        if not result:
+                            path["status"] = f"Error in process {path["name"]}. See logs for more detailed error message."
+                else:
+                    file["backup_paths"][entry]["last_backup"] = now.isoformat()
+                    save(file)
+            time.sleep(60)
+        else:
+            time.sleep(5)
 
 def start_backup():
-    thread = threading.Thread(target=backup, daemon=True)
+    thread = threading.Thread(target=check_for_backup, daemon=True)
     thread.start()
+
+def check_for_data_file():
+    global data_file_path
+    if not data_file_path.exists():
+        default_content = {
+            "backup_paths": []
+        }
+        with open(data_file_path, 'w', encoding='utf-8') as f:
+            json.dump(default_content, f, indent=4)
 
 @app.route("/")
 def home():
@@ -112,6 +95,51 @@ def home():
     backup_paths = file["backup_paths"]
     return render_template("index.html", backup_paths=backup_paths)
 
+@app.route("/create_backup_task", methods=["POST"])
+def create_backup_task():
+    name = request.form.get("name")
+    folder_to_backup = request.form.get("folder_to_backup")
+    folder_to_save_backup = request.form.get("folder_to_save_backup")
+    backup_frequency = request.form.get("backup_frequency")
+    entry = {
+        "folder_to_backup": folder_to_backup,
+        "folder_to_save_backup": folder_to_save_backup,
+        "name": name,
+        "last_backup": "",
+        "backup_frequency": backup_frequency,
+        "status": "ok"
+    }
+    file = read()
+    file["backup_paths"].append(entry)
+    save(file)
+    return redirect(url_for("home"))
+
+@app.route("/edit_backup_task", methods=["POST"])
+def edit_backup_task():
+    name = request.form.get("name")
+    folder_to_backup = request.form.get("folder_to_backup")
+    folder_to_save_backup = request.form.get("folder_to_save_backup")
+    backup_frequency = request.form.get("backup_frequency")
+    file = read()
+    for x, entry in enumerate(file["backup_paths"]):
+        if entry["name"] == name:
+            last_backup = entry["last_backup"]
+            status = entry["status"]
+            entry = {
+                "folder_to_backup": folder_to_backup,
+                "folder_to_save_backup": folder_to_save_backup,
+                "name": name,
+                "last_backup": last_backup,
+                "backup_frequency": backup_frequency,
+                "status": status
+            }
+
+            file["backup_paths"][x] = entry
+            save(file)
+
 if __name__ == "__main__":
+    check_for_data_file()
     start_backup()
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+#TODO: checken, ob Pfad valid ist, wenn ich, in status pushen
